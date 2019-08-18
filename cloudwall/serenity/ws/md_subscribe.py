89c709e822bdbe8e15a723cab1f156c2e73e01f2
Kyle Downey <kyle.downey@gmail.com>
2019-08-18 09:04:45 -0400

    Switch to using Tornado event loop

--- cloudwall/serenity/ws/md_subscribe.py
+++ cloudwall/serenity/ws/md_subscribe.py
@@ -1,16 +1,23 @@
 import capnp
 import zmq
+from zmq.eventloop import ioloop
+from zmq.eventloop.zmqstream import ZMQStream
 
 capnp.remove_import_hook()
 fh_capnp = capnp.load('../../../../serenity-fh/capnp/serenity-fh.capnp')
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-
 socket.connect("tcp://localhost:5556")
 socket.setsockopt_string(zmq.SUBSCRIBE, "")
+stream = ZMQStream(socket)
 
-while True:
-    msg_bytes = socket.recv()
-    msg = fh_capnp.TradeMessage.from_bytes(msg_bytes)
+
+def on_message(msgs):
+    msg = fh_capnp.TradeMessage.from_bytes(msgs[0])
     print("{} {} @ {}".format(msg.side, msg.size, msg.price))
+
+
+stream.on_recv(on_message)
+
+ioloop.IOLoop.instance().start()
